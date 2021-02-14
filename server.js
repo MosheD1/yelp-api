e2db7a288d0a5ad3da18bc51bf2eba49e93563e0
Moshe Dego <moshedego@gmail.com>
2021-02-14 11:58:40 +0200

    conect server to a DB and update all routes

--- server.js
+++ server.js
@@ -1,80 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex');
 const bcrypt = require('bcrypt');
 const salt = 10;
-
-const database = {
-    users: [
-        {
-            id: '1',
-            name: 'moshe',
-            email: 'moshe@gmail.com',
-            password: '123',
-            joined: new Date()
-        },
-        {
-            id: '2',
-            name: 'aviv',
-            email: 'aviv@gmail.com',
-            password: '123',
-            joined: new Date()
-        },
-        {
-            id: '3',
-            name: 'gili',
-            email: 'gili@gmail.com',
-            password: '123',
-            joined: new Date()
-        },
-        {
-            id: '4',
-            name: 'eli',
-            email: 'eli@gmail.com',
-            password: '123',
-            joined: new Date()
-        }
-    ],
-    busniesses: [
-        {
-            id: '1',
-            name: 'moshe\'s place',
-            country: 'israel',
-            city: 'rehovot',
-            street: 'Herzel 123',
-            picture: 'https://images.unsplash.com/photo-1609253119912-b3cee27b7ef8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-            category: 'food',
-            description: 'best food in town, there are many great dishes, professional staff and great prices',
-            phoneNumber: '054-85963XX'
-        },
-        {
-            id: '2',
-            name: 'Surf School',
-            country: 'israel',
-            city: 'tel-aviv',
-            street: 'Herzel 543',
-            picture: 'https://images.unsplash.com/photo-1609253119912-b3cee27b7ef8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-            category: 'sport',
-            description: 'best surf school in town, there are many great teacher, great weather and fun activities',
-            phoneNumber: '052-85963XX'
-        }
-    ],
-    reviews: [
-        {
-            id: '1',
-            businessId: '1',
-            name: 'moshe',
-            content: 'great restaurant, go check it out',
-            rating: 0
-        },
-        {
-            id: '2',
-            businessId: '2',
-            name: 'moshe',
-            content: 'one of the best surf schools in the world',
-            rating: 0
-        }
-    ]
-};
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'test',
+      database : 'yelp'
+    }
+});
 
 const app = express();
 
@@ -89,42 +26,69 @@ app.get('/', (req, res) => {
 // post signin
 app.post('/signin', (req, res) => {
     const { password, email } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if(user.email === email && user.password === password) {
-            found = true;
-            return res.status(200).json(user);
+    db.select('email', 'hash').from('login')
+    .where({
+        email: email
+    })
+    .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        if(isValid) {
+            return db.select('*').from('users')
+            .where({
+                email: data[0].email
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+            .catch(err => res.status(400).json('User Not Found'));
+        } else {
+            res.status(400).json('wrong credentials');
         }
-    });
-
-    if(!found) {
-        res.status(404).json('User Not Found');
-    }
+    })
+    .catch(err => res.status(400).json('wrong credentials'));
 });
 
 // post register
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    database.users.push({
-        id: database.users.length + 1,
-        name: name,
-        email: email,
-        password: password,
-        joined: new Date()
-    });
-
-    res.status(200).json(database.users[database.users.length - 1]);
+    const hash = bcrypt.hashSync(password, salt);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: loginEmail[0],
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(404).json('unable to register'));
 });
 
 // get businesses
 app.get('/businesses', (req, res) => {
-    res.json(database.busniesses);
+    db.select('*').from('businesses')
+    .then(busniesses => res.json(busniesses))
+    .catch(() => res.status(400).json('unable to retrive data'));
 });
 
 // post add business
 app.post('/business', (req, res) => {
-    database.busniesses.push({
-        id: `${database.busniesses.length + 1}`,
+    db('businesses')
+    .returning('*')
+    .insert({
         name: req.body.name,
         country: req.body.country,
         city: req.body.city,
@@ -132,51 +96,54 @@ app.post('/business', (req, res) => {
         picture: req.body.picture,
         category: req.body.category,
         description: req.body.description,
-        phoneNumber: req.body.phoneNumber
+        phonenumber: req.body.phoneNumber
+    })
+    .then(business => res.json(business[0]))
+    .catch(err => {
+        res.status(400).json('unable to add new business');
     });
-
-    res.json(database.busniesses[database.busniesses.length - 1]);
 });
 
 // get buisness
 app.get('/business/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.busniesses.forEach(b => {
-        if(id === b.id) {
-            found = true;
-            return res.json(b);
+    db.select('*').from('businesses')
+    .where({ id })
+    .then(business => {
+        if(business.length) {
+            res.json(business[0]);
+        } else {
+             res.status(404).json('not found');
         }
-    });
-
-    if(!found) {
-        return res.status(404).json('not found');
-    }
+    })
+    .catch(err => res.status(400).json('error getting business'));
 });
 
 // post review
 app.post('/review/:businessId', (req, res) => {
     const { businessId } = req.params;
-    database.reviews.push({
-        id: `${database.reviews.length + 1}`,
-        businessId: businessId,
+    db('reviews')
+    .returning('*')
+    .insert({
+        businessid: businessId,
         name: req.body.name,
-        content: req.body.content
-
-    });
-
-    const updatedReviews = database.reviews.filter(r => r.businessId === businessId);
-    res.json(updatedReviews);
+        content: req.body.content,
+        rating: req.body.rating
+    })
+    .then(review => {
+        return db.select('*').from('reviews').where({ businessid: review[0].businessid })
+        .then(reviews => res.json(reviews))
+    })
+    .catch(err => res.status(400).json('unable to post review'));
 });
 
 // get reviews of business
 app.get('/review/:businessId', (req, res) => {
     const { businessId } = req.params;
-    const reviews = database.reviews.filter(c => {
-        return businessId === c.businessId;
-    });
-
-    res.json(reviews);
+    db.select('*').from('reviews')
+    .where({ businessid: businessId })
+    .then(reviews => res.json(reviews))
+    .catch(err => res.status(400).json('unable to retrive reviews'));
 });
 
 app.listen(3001, () => {
